f395cbcb0a8931c343dbfb688892d508d9a4d46e
arvidn <arvid@libtorrent.org>
2021-08-19 06:50:55 +0200

    use run_generator2 rust call and compact spend bundle conditions data structure pervasively.

--- chia/consensus/cost_calculator.py
+++ chia/consensus/cost_calculator.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Optional
 
-from chia.types.name_puzzle_condition import NPC
-from chia.util.ints import uint16, uint64
+from chia.util.ints import uint64, uint16
 from chia.util.streamable import Streamable, streamable
+from chia.types.spend_bundle_conditions import SpendBundleConditions
 
 
 @dataclass(frozen=True)
 @streamable
 class NPCResult(Streamable):
     error: Optional[uint16]
-    npc_list: List[NPC]
+    conds: Optional[SpendBundleConditions]
     cost: uint64  # The total cost of the block, including CLVM cost, cost of
     # conditions and cost of bytes
