83bf050d8c1c4b30b38994782f4eb18056c39cc1
arvidn <arvid@libtorrent.org>
2022-02-11 14:58:50 +0100

    fix db_upgrade test. close the database connection before running the conversion function (to correctly test offline mode). Speed up the test by using a pre-created blockchain

--- tests/core/test_db_conversion.py
+++ tests/core/test_db_conversion.py
@@ -2,20 +2,21 @@ import pytest
 import aiosqlite
 import tempfile
 import random
+import asyncio
 from pathlib import Path
 from typing import List, Tuple
 
-from tests.blockchain.blockchain_test_utils import _validate_and_add_block
-from tests.setup_nodes import bt, test_constants
+from tests.setup_nodes import test_constants
 
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.util.ints import uint32
+from chia.util.ints import uint32, uint64
 from chia.cmds.db_upgrade_func import convert_v1_to_v2
 from chia.util.db_wrapper import DBWrapper
 from chia.full_node.block_store import BlockStore
 from chia.full_node.coin_store import CoinStore
 from chia.full_node.hint_store import HintStore
 from chia.consensus.blockchain import Blockchain
+from chia.consensus.multiprocess_validation import PreValidationResult
 
 
 class TempFile:
@@ -38,12 +39,18 @@ def rand_bytes(num) -> bytes:
     return bytes(ret)
 
 
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+
+
 class TestDbUpgrade:
     @pytest.mark.asyncio
     @pytest.mark.parametrize("offline, vacuum", [(True, True), (True, False), (False, False)])
-    async def test_blocks(self, offline: bool, vacuum: bool):
+    async def test_blocks(self, default_1000_blocks, offline: bool, vacuum: bool):
 
-        blocks = bt.get_consecutive_blocks(758)
+        blocks = default_1000_blocks
 
         hints: List[Tuple[bytes32, bytes]] = []
         for i in range(351):
@@ -68,11 +75,17 @@ class TestDbUpgrade:
         with TempFile() as in_file, TempFile() as out_file:
 
             async with aiosqlite.connect(in_file) as conn:
+
+                await conn.execute("pragma journal_mode=OFF")
+                await conn.execute("pragma synchronous=OFF")
+                await conn.execute("pragma locking_mode=exclusive")
+
                 db_wrapper1 = DBWrapper(conn, 1)
                 block_store1 = await BlockStore.create(db_wrapper1)
                 coin_store1 = await CoinStore.create(db_wrapper1, uint32(0))
                 hint_store1 = await HintStore.create(db_wrapper1)
 
+                print("adding hints to database")
                 for h in hints:
                     await hint_store1.add_hints([(h[0], h[1])])
 
@@ -81,52 +94,58 @@ class TestDbUpgrade:
                 )
                 await db_wrapper1.commit_transaction()
 
+                print("adding blocks to database")
                 for block in blocks:
-                    await _validate_and_add_block(bc, block)
+                    # await _validate_and_add_block(bc, block)
+                    results = PreValidationResult(None, uint64(1), None, False)
+                    result, err, _, _ = await bc.receive_block(block, results)
+                    assert err is None
 
-                # now, convert v1 in_file to v2 out_file
-                await convert_v1_to_v2(in_file, out_file, offline=offline, vacuum=vacuum)
+            # now, convert v1 in_file to v2 out_file
+            await convert_v1_to_v2(in_file, out_file, offline=offline, vacuum=vacuum)
 
-                async with aiosqlite.connect(out_file) as conn2:
-                    db_wrapper2 = DBWrapper(conn2, 2)
-                    block_store2 = await BlockStore.create(db_wrapper2)
-                    coin_store2 = await CoinStore.create(db_wrapper2, uint32(0))
-                    hint_store2 = await HintStore.create(db_wrapper2)
+            async with aiosqlite.connect(in_file) as conn, aiosqlite.connect(out_file) as conn2:
 
-                    # check hints
-                    for h in hints:
-                        assert h[0] in await hint_store1.get_coin_ids(h[1])
-                        assert h[0] in await hint_store2.get_coin_ids(h[1])
+                db_wrapper1 = DBWrapper(conn, 1)
+                block_store1 = await BlockStore.create(db_wrapper1)
+                coin_store1 = await CoinStore.create(db_wrapper1, uint32(0))
+                hint_store1 = await HintStore.create(db_wrapper1)
 
-                    # check peak
-                    assert await block_store1.get_peak() == await block_store2.get_peak()
+                db_wrapper2 = DBWrapper(conn2, 2)
+                block_store2 = await BlockStore.create(db_wrapper2)
+                coin_store2 = await CoinStore.create(db_wrapper2, uint32(0))
+                hint_store2 = await HintStore.create(db_wrapper2)
 
-                    # check blocks
-                    for block in blocks:
-                        hh = block.header_hash
-                        height = block.height
-                        assert await block_store1.get_full_block(hh) == await block_store2.get_full_block(hh)
-                        assert await block_store1.get_full_block_bytes(hh) == await block_store2.get_full_block_bytes(
-                            hh
-                        )
-                        assert await block_store1.get_full_blocks_at([height]) == await block_store2.get_full_blocks_at(
-                            [height]
-                        )
-                        assert await block_store1.get_block_records_by_hash(
-                            [hh]
-                        ) == await block_store2.get_block_records_by_hash([hh])
-                        assert await block_store1.get_block_record(hh) == await block_store2.get_block_record(hh)
-                        assert await block_store1.is_fully_compactified(hh) == await block_store2.is_fully_compactified(
-                            hh
-                        )
+                # check hints
+                for h in hints:
+                    assert h[0] in await hint_store1.get_coin_ids(h[1])
+                    assert h[0] in await hint_store2.get_coin_ids(h[1])
 
-                    # check coins
-                    for block in blocks:
-                        coins = await coin_store1.get_coins_added_at_height(block.height)
-                        assert await coin_store2.get_coins_added_at_height(block.height) == coins
-                        assert await coin_store1.get_coins_removed_at_height(
-                            block.height
-                        ) == await coin_store2.get_coins_removed_at_height(block.height)
-                        for c in coins:
-                            n = c.coin.name()
-                            assert await coin_store1.get_coin_record(n) == await coin_store2.get_coin_record(n)
+                # check peak
+                assert await block_store1.get_peak() == await block_store2.get_peak()
+
+                # check blocks
+                for block in blocks:
+                    hh = block.header_hash
+                    height = block.height
+                    assert await block_store1.get_full_block(hh) == await block_store2.get_full_block(hh)
+                    assert await block_store1.get_full_block_bytes(hh) == await block_store2.get_full_block_bytes(hh)
+                    assert await block_store1.get_full_blocks_at([height]) == await block_store2.get_full_blocks_at(
+                        [height]
+                    )
+                    assert await block_store1.get_block_records_by_hash(
+                        [hh]
+                    ) == await block_store2.get_block_records_by_hash([hh])
+                    assert await block_store1.get_block_record(hh) == await block_store2.get_block_record(hh)
+                    assert await block_store1.is_fully_compactified(hh) == await block_store2.is_fully_compactified(hh)
+
+                # check coins
+                for block in blocks:
+                    coins = await coin_store1.get_coins_added_at_height(block.height)
+                    assert await coin_store2.get_coins_added_at_height(block.height) == coins
+                    assert await coin_store1.get_coins_removed_at_height(
+                        block.height
+                    ) == await coin_store2.get_coins_removed_at_height(block.height)
+                    for c in coins:
+                        n = c.coin.name()
+                        assert await coin_store1.get_coin_record(n) == await coin_store2.get_coin_record(n)
